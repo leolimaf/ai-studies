b6984bf0d0c5f1ddd96996df8cd87ef4c8598e28
Leonardo Lima <leonardolimaf54@gmail.com>
2026-02-05 18:23:25 -0300

    console integrado ao ollama utilizado para testar alguns recursos so semantic kernel

---
+++ SemanticKernel.Console/Plugins/ProductPlugin.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+using Microsoft.SemanticKernel;
+
+namespace SemanticKernel.Console.Plugins;
+
+public class ProductPlugin
+{
+    private readonly List<Product> _products =
+    [
+        new(1, "Laptop", true, 999.99m),
+        new(2, "Smartphone", false, 499.99m),
+        new(3, "Tablet", true, 299.99m)
+    ];
+    
+    [KernelFunction("get_products")]
+    [Description("Returns a list of products")]
+    public async Task <List<Product>> GetProductsAsync()
+    {
+        await Task.Delay(1);
+        return _products.ToList();
+    }
+    
+    [KernelFunction("get_product_by_id")]
+    [Description("Returns a product by its ID")]
+    public async Task<Product?> GetProductByIdAsync(int id)
+    {
+        await Task.Delay(1);
+        return _products.FirstOrDefault(p => p.Id == id);
+    }
+}
+
+public record Product(int Id, string Name, bool IsActive, decimal Price);
---
+++ SemanticKernel.Console/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.Ollama;
+using SemanticKernel.Console.Plugins;
+
+var builder = Kernel.CreateBuilder()
+    .AddOllamaChatCompletion(modelId: "llama3.1:latest", endpoint: new Uri("http://localhost:11434"));
+    
+// builder.Services.AddLogging(x => x.AddConsole().SetMinimumLevel(LogLevel.Trace));
+
+var app = builder.Build();
+
+var chatCompletionService = app.GetRequiredService<IChatCompletionService>();
+app.Plugins.AddFromType<ProductPlugin>(nameof(ProductPlugin));
+
+OllamaPromptExecutionSettings executionSettings = new() { FunctionChoiceBehavior = FunctionChoiceBehavior.Auto() };
+
+var history = new ChatHistory();
+
+string? input;
+
+do
+{
+    Console.Write(">> ");
+    input = Console.ReadLine() ?? string.Empty;
+    
+    history.AddUserMessage(input);
+
+    var result = await chatCompletionService.GetChatMessageContentAsync(history, executionSettings, kernel: app);
+    Console.WriteLine(result);
+    
+    history.AddMessage(result.Role, result.Content ?? string.Empty);
+} while (!string.IsNullOrEmpty(input));
